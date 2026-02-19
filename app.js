1b9a85c1aa92c32a284e42466ac487adec77d41c
Atharv Patil <atharvapatil2542@gmail.com>
2026-02-19 15:38:10 +0530

    Add Project Files

---
+++ app.js
@@ -0,0 +1,120 @@
+if(process.env.NODE_ENV != "production"){
+    require("dotenv").config();
+}
+
+const  express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema,reviewSchema}  = require("./schema.js");
+const Review = require("./models/review.js");
+const session = require("express-session");
+const connectMongo = require('connect-mongo');
+let MongoStore = connectMongo;
+if (connectMongo && connectMongo.default) MongoStore = connectMongo.default;
+if (MongoStore && typeof MongoStore.create !== 'function') {
+    if (MongoStore.MongoStore && typeof MongoStore.MongoStore.create === 'function') MongoStore = MongoStore.MongoStore;
+}
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
+
+
+
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
+
+
+
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
+
+const dbUrl = process.env.ATLASDB_URL
+main().then(() =>{
+    console.log("Connected to DB");
+}).catch((err) =>{
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect(dbUrl);
+}
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: process.env.SECRET,
+    },
+    touchAfter: 24 * 3600,
+});
+
+store.on("error",() =>{
+    console.log("ERROR in MONGO SESSION  STORE",err);
+});
+
+const sessionOptions = {
+    store,
+    secret : process.env.SECRET , 
+    resave:false ,
+    saveUninitialized : true,
+    cookie : {
+        expires : Date.now() + 7 * 24 * 60 *60 * 1000,
+        maxAge : 7 * 24 * 60 *60 * 1000,
+        httpOnly : true,
+    },
+};
+
+
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req,res,next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    res.locals.currUser = req.user;
+    next();
+});
+
+
+//Post
+//User route
+app.use("/listings",listingRouter);
+
+// Reviews
+//Post Review route
+app.use("/listings/:id/reviews",reviewRouter);
+app.use("/",userRouter);
+
+app.use((req,res,next) =>{
+    next(new ExpressError(404,"Page not found!"));
+});
+
+app.use((err,req,res,next) =>{
+    let {statusCode = 500,message = "Something went wrong!"} = err;
+    res.status(statusCode).render("error.ejs",{message});
+    //res.status(statusCode).send(message);
+})
+
+app.listen(8080,() =>{
+    console.log("Server is listen to port 8080");
+});
---
+++ routes/listing.js
@@ -0,0 +1,61 @@
+const  express = require("express");
+const router = express.Router({mergeParams : true});
+const wrapAsync = require("../utils/wrapAsync.js");
+const ExpressError = require("../utils/ExpressError.js");
+const {listingSchema,reviewSchema}  = require("../schema.js");
+const Listing = require("../models/listing.js");
+const {isLoggedIn} = require("../middleware.js"); 
+const listingController = require("../controllers/listing.js");
+const multer = require('multer');
+const {storage} = require("../cloudConfig.js");
+const upload = multer({ storage})
+
+
+const validateListing = (req,res,next) =>{
+    let {error} = listingSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
+};
+
+
+
+
+router.route("/").get(wrapAsync(listingController.index))
+.post(isLoggedIn,upload.single("image"),validateListing,wrapAsync(listingController.createListing));
+
+// New Route 
+router.get("/new",isLoggedIn,listingController.renderNewForm);
+
+router.route("/:id").get(wrapAsync(listingController.showListing))
+.put(isLoggedIn,upload.single("image"),validateListing,wrapAsync(listingController.updateListing))
+.delete(isLoggedIn,wrapAsync(listingController.destroyListing));
+
+
+
+// Index Route
+//router.get("/",wrapAsync(listingController.index));
+
+
+
+// Show Route 
+//router.get("/:id",wrapAsync(listingController.showListing));
+ 
+//Create Route
+//router.post("/",isLoggedIn,validateListing,wrapAsync(listingController.createListing));
+
+// Edit Route
+router.get("/:id/edit",isLoggedIn,wrapAsync(listingController.renderEditForm));
+
+// Update Route
+//router.put("/:id",isLoggedIn,validateListing,wrapAsync(listingController.updateListing));
+
+// Delete Route 
+//router.delete("/:id",isLoggedIn,wrapAsync(listingController.destroyListing));
+
+
+
+module.exports = router;
